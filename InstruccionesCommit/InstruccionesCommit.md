5d5dae8b1f18c1f84c0577dd50524e31bec3c08f
Marco Perdomo <marcooperdomo@gmail.com>
2020-06-06 23:42:12 -0500

    Paginación personalizada

--- InstruccionesCommit/InstruccionesCommit.md
+++ InstruccionesCommit/InstruccionesCommit.md
@@ -4,7 +4,7 @@
 <!-- end title -->
 
 <!-- commit name -->
-### Commit | __Posts con vídeo y audio__
+### Commit | __Paginación personalizada__
 <!-- end commit name -->
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -17,32 +17,27 @@
 
 <!-- commit instructions -->
 #### Instrucciones Commit
-1. Edición de la vista `resources\views\welcome.blade.php`
-2. Edición de la migración `database\migrations\2020_05_09_170826_create_posts_table.php`
-3. Rehacer las migraciones con seeds
-   > php artisan migrate:refresh --seed
-4. Edición de la vista `resources\views\admin\posts\edit.blade.php`
-5. Edición del controlador `app\Http\Controllers\Admin\PostsController.php`
-   - Edición de la función `update(Post $post, Request $request)`
-6. Edición de la vista `resources\views\posts\show.blade.php`
+1. Edición del controlador `app\Http\Controllers\PagesController.php`
+   - Edición de la función `home()`
+2. Edición de la vista `resources\views\welcome.blade.php`
+3. Para editar el diseño de la paginación que Laravel incluye por defecto
+   > php artisan vendor:publish --tag=laravel-pagination
+   - Edición de la vista `resources\views\vendor\pagination\default.blade.php`
 <!-- end commit instructions -->
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 <!-- notes -->
 #### Notas:
-- [Documentación | `Ataques XSS - Displaying Unescaped Data`](https://laravel.com/docs/5.4/blade#displaying-data)
 <!-- end notes -->
 
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 <!-- information -->
 #### Información:
+- Más información en `app\Http\Controllers\PagesController.php`
+
 - Más información en `resources\views\welcome.blade.php`
 
-- Más información en `resources\views\admin\posts\edit.blade.php`
-
-- Más información en `app\Http\Controllers\Admin\PostsController.php`
-
-- Más información en `resources\views\posts\show.blade.php`
+- Más información en `resources\views\vendor\pagination\default.blade.php`
 <!-- end information -->
--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,42 +7,14 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    /* 
-        | --------------------------------------------------------------------------------------------------
-        | *Función que devuelve a todos los posts ordenados por el campo 'published_at' de forma descendente 
-        | *$posts = Post::all(); Obtiene todos los posts
-        | *Devuelve la vista resources\views\welcome.blade.php y le pasa la variable $posts
-        | *No olvidar importar el modelo use App\Post;
-        | --------------------------------------------------------------------------------------------------
-    */
     public function home()
     {
         /* 
-            | ------------------------------------------------------------------------------------
-            | *Obtiene a todos los posts de forma descendente omitiendo los posts sin fecha(nulos)
-            | ------------------------------------------------------------------------------------
+        | -----------------------------------------------------------------------------------------------
+        | *->paginate() - Al no colocar un número en la paginación Laravel paginará de 15 en 15 registros
+        | -----------------------------------------------------------------------------------------------
         */
-        // $posts = Post::whereNotNull('published_at')
-        //         ->latest('published_at')
-        //         ->get();
-
-        /* 
-            | ----------------------------------------------------------------------------------------------
-            | *Obtiene a todos los posts de forma descendente omitiendo los posts sin fecha(nulos) o futuros
-            | ----------------------------------------------------------------------------------------------
-        */
-        // $posts = Post::whereNotNull('published_at')
-        //         ->where('published_at', '<=', Carbon::now())
-        //         ->latest('published_at')
-        //         ->get();
-
-        /* 
-            | -----------------------------------------------------------------------------------------------
-            | *Obtiene el QueryScope published() de la función scopePublished($query) del modelo app\Post.php
-            |   *El nombre de la queryscope es scopePublished() pero al llamarlo solo se usa published()
-            | -----------------------------------------------------------------------------------------------
-        */
-        $posts = Post::published()->get();
+        $posts = Post::published()->paginate(4);
         return view('welcome', compact('posts'));
     }
 }
